b7f2ba05749a1dd1880282e92a045282b5642159
Nandini-Maheshwar <nandinim1204@gmail.com>
2024-03-01 00:05:38 +0530

    Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,24 +2,54 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation"; //updated
-import React from "react";
-import { axios } from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   })
-  const onLogin = async () => {}
+
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      toast.error(error.message);
+
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if(user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  })
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Login</h1>
+      <h1>{loading ? "processing" : "login"}</h1>
       <hr />
 
       <label htmlFor="email">email</label>
       <input 
       className="p-2 border border-gray-300 rounded-lg mb-4
-      focus:outline-none focus:border-gray-600"
+      focus:outline-none focus:border-gray-600 text-black"
         id="email"
         type="text"
         value={user.email}
@@ -30,7 +60,7 @@ export default function LoginPage() {
       <label htmlFor="password">password</label>
       <input 
       className="p-2 border border-gray-300 rounded-lg mb-4
-      focus:outline-none focus:border-gray-600"
+      focus:outline-none focus:border-gray-600 text-black"
         id="password"
         type="text"
         value={user.password}
